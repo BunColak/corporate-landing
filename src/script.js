02d622dbd8e2e5f8eff02f2a4cf17d68ef93ae80
Bun Colak <bun.colak@motius.de>
2020-10-11 19:58:55 +0200

    accordion

--- src/script.js
+++ src/script.js
@@ -1,13 +1,26 @@
-const mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 30,
-  centeredSlides: true,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
+$(document).ready(function () {
+  const mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
     },
-  },
+  });
+
+  const accordionItems = $(".accordion__title");
+  const accordionContents = $(".accordion__content");
+  accordionItems.on("click", function () {
+    const item = $(this);
+    item.find("i").toggleClass("open"); // Rotate chevron
+    const content = item.siblings(".accordion__content");
+    accordionContents.not(content).slideUp();
+
+    content.slideToggle();
+  });
 });
