091f2bb718e01b8051f8a6d5deb04d40b53fbb6e
Bun Colak <bun.colak@motius.de>
2020-10-11 14:01:07 +0200

    testimonials

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-console.log('Hello there')
+const mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
